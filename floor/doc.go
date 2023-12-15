e0aab5c20bc530ecbe22139db568bc5e87dad212
Benjamin LECOMTE <benjamin.lecomte@etu.univ-nantes.fr>
2023-12-15 19:18:07 +0100

    update doc

--- floor/doc.go
+++ floor/doc.go
@@ -4,5 +4,44 @@ personnage se déplace. Sa principale fonction est de fournir, chaque
 1/60 de seconde et en fonction de la position absolue de la caméra,
 une structure de données représentant les cases de terrain visibles
 à l'écran.
+
+# TYPES
+
+	type Floor struct {
+	        // Has unexported fields.
+	}
+
+	Floor représente les données du terrain. Pour le moment aucun champs n'est
+	exporté.
+
+	  - content : partie du terrain qui doit être affichée à l'écran
+	  - fullContent : totalité du terrain (utilisé seulement avec le type
+	    d'affichage du terrain "fromFileFloor")
+	  - quadTreeContent : totalité du terrain sous forme de quadtree (utilisé
+	    avec le type d'affichage du terrain "quadtreeFloor")
+
+func (f Floor) Blocking(characterXPos, characterYPos, camXPos, camYPos int) (blocking [4]bool)
+
+	Blocking retourne, étant donnée la position du personnage, un tableau de
+	booléen indiquant si les cases au dessus (0), à droite (1), au dessous (2)
+	et à gauche (3) du personnage sont bloquantes.
+
+func (f Floor) Draw(screen *ebiten.Image)
+
+	Draw affiche dans une image (en général, celle qui représente l'écran), la
+	partie du sol qui est visible (qui doit avoir été calculée avec Get avant).
+
+func (f *Floor) Init()
+
+	Init initialise les structures de données internes de f
+
+func (f *Floor) Update(camXPos, camYPos int)
+
+	Update se charge de stocker dans la structure interne (un tableau) de f une
+	représentation de la partie visible du terrain à partir des coordonnées
+	absolues de la case sur laquelle se situe la caméra.
+
+	On aurait pu se passer de cette fonction et tout faire dans Draw. Mais cela
+	permet de découpler le calcul de l'affichage.
 */
 package floor
